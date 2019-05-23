d35df81ce60c7933c780065bf7b82292c58ff682
Breezy Fasano <breezyfasano@gmail.com>
2019-05-23 10:48:21 -0500

    basic list functionality and OOP logic

--- scripts.js
+++ scripts.js
@@ -1,34 +1,38 @@
-let todos = [];
-
-function addItem(item) {
-  const ul = document.querySelector(".todo");
-  ul.innerHTML = "";
-  todos.push(item);
-}
-
-function removeItem(index) {
-  todos.pop(index);
-}
-
-function editItem(index, input) {
-  todos[index] = input;
-}
-
-function displayList() {
-  todos.forEach(item => {
-    // for each to do item, display it as a li
-    const ul = document.querySelector(".todo");
-    let li = document.createElement("li");
-    li.textContent = item;
-    ul.appendChild(li);
-  });
-}
+'use strict';
 
 const itemInput = document.querySelector("#inputNewToDo");
 const inputButton = document.querySelector(".save");
 
-inputButton.addEventListener("click", function() {
-  addItem(itemInput.value);
-  displayList();
-  itemInput.value = '';
+let app = {
+  todos: [],
+  addItem: function (item) {
+    const ul = document.querySelector(".todo-list");
+    ul.innerHTML = "";
+    this.todos.push(item + " ");
+  },
+  removeItem: function(index) {
+    this.todos.pop(index);
+  },
+  editItem: function(index, input) {
+    this.todos[index] = input;
+  },
+  displayList: function() {
+    this.todos.forEach(item => {
+      // for each to do item, display it as a li
+      const ul = document.querySelector(".todo-list");
+      let li = document.createElement("li");
+      li.innerHTML = item;
+      let checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      ul.appendChild(li);
+      li.appendChild(checkbox);
+    });
+  },
+}
+
+inputButton.addEventListener("click", function () {
+  app.addItem(itemInput.value); //adds whatever is in input box
+  app.displayList(); // refreshes list
+  itemInput.value = ""; // clears out input box
 });
+
