e332eab8ab2a00959faa19958e42bed434d18f90
Breezy Fasano <breezyfasano@gmail.com>
2019-05-29 13:54:19 -0500

    New to do can be added with enter key

--- scripts.js
+++ scripts.js
@@ -3,37 +3,48 @@
 const itemInput = document.querySelector("#inputNewToDo");
 const inputButton = document.querySelector(".save");
 
+function sanitizeString(str){
+  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+  return str.trim();
+}
+
 let app = {
   todos: [],
-  addItem: function (item) {
+  addItem: function(item) {
     const ul = document.querySelector(".todo-list");
     ul.innerHTML = "";
-    this.todos.push(item);
+    if (item !== "") {
+      this.todos.push(item);
+    } else { alert("A todo item can not be blank!")};
+    this.displayList(); // refreshes list
+    itemInput.value = ""; // clears out input box
   },
   displayList: function () {
     this.todos.forEach(item => {
       // for each to do item, display it as a li
       const ul = document.querySelector(".todo-list");
-      let text = `<li><i id="checkbox" class="far fa-circle" onclick="app.removeItem()"></i> ${item}</li>`;
+      let text = `<li><i id="checkbox" class="far fa-circle" onclick="app.removeItem()"></i> ${sanitizeString(item)}</li>`;
       ul.insertAdjacentHTML("beforeend", text)
     });
   },
 }
 
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function(e) {
   if (e.target && e.target.id == 'checkbox') {
     if (e.target.classList.contains('fa-circle')) {
-      // e.target.classList.toggle('fa-circle');
       e.target.classList.toggle('fa-check-circle');
     } else if (e.target.classList.contains('fa-check-circle')) {
-      // e.target.classList.toggle('fa-check-circle');
       e.target.classList.toggle('fa-circle');
     }
   }
 });
 
-inputButton.addEventListener('click', function () {
+inputButton.addEventListener('click', function() {
   app.addItem(itemInput.value); //adds whatever is in input box
-  app.displayList(); // refreshes list
-  itemInput.value = ""; // clears out input box
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode == 13) {
+    app.addItem(itemInput.value)
+  }
 });
