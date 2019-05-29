d609e58be74ddf6e021cff29ec2f4c13c5025df7
Breezy Fasano <breezyfasano@gmail.com>
2019-05-29 13:54:55 -0500

    add new todo pressing enter

--- scripts.js
+++ scripts.js
@@ -3,36 +3,48 @@
 const itemInput = document.querySelector("#inputNewToDo");
 const inputButton = document.querySelector(".save");
 
+function sanitizeString(str){
+  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+  return str.trim();
+}
+
 let app = {
   todos: [],
-  addItem: function (item) {
+  addItem: function(item) {
     const ul = document.querySelector(".todo-list");
     ul.innerHTML = "";
-    this.todos.push(item + " ");
+    if (item !== "") {
+      this.todos.push(item);
+    } else { alert("A todo item can not be blank!")};
+    this.displayList(); // refreshes list
+    itemInput.value = ""; // clears out input box
   },
-  removeItem: function(index) {
-    this.todos.pop(index);
-  },
-  editItem: function(index, input) {
-    this.todos[index] = input;
-  },
-  displayList: function() {
+  displayList: function () {
     this.todos.forEach(item => {
       // for each to do item, display it as a li
       const ul = document.querySelector(".todo-list");
-      let li = document.createElement("li");
-      li.innerHTML = item;
-      let checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      ul.appendChild(li);
-      li.appendChild(checkbox);
+      let text = `<li><i id="checkbox" class="far fa-circle" onclick="app.removeItem()"></i> ${sanitizeString(item)}</li>`;
+      ul.insertAdjacentHTML("beforeend", text)
     });
   },
 }
 
-inputButton.addEventListener("click", function () {
-  app.addItem(itemInput.value); //adds whatever is in input box
-  app.displayList(); // refreshes list
-  itemInput.value = ""; // clears out input box
+document.addEventListener('click', function(e) {
+  if (e.target && e.target.id == 'checkbox') {
+    if (e.target.classList.contains('fa-circle')) {
+      e.target.classList.toggle('fa-check-circle');
+    } else if (e.target.classList.contains('fa-check-circle')) {
+      e.target.classList.toggle('fa-circle');
+    }
+  }
 });
 
+inputButton.addEventListener('click', function() {
+  app.addItem(itemInput.value); //adds whatever is in input box
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode == 13) {
+    app.addItem(itemInput.value)
+  }
+});
