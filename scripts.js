8a094139174556cad0bb44655f285b5f15c718bc
Breezy Fasano <breezyfasano@gmail.com>
2019-06-20 14:23:58 -0500

    Fixed a bug where if you added another to do item after checking others off, all other items would uncheck themselves and reset

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 'use strict';
 
+//variables & elements
 const itemInput = document.querySelector("#inputNewToDo");
+const clearButton = document.querySelector('.clear');
 const inputButton = document.querySelector(".save");
 const ul = document.querySelector(".todo-list");
+const input = document.getElementById('inputNewToDo');
+const addIcon = '<i class="fas fa-plus" style="color: white;"></i> ';
+
+const checked = "fa-check-circle";
+const unchecked = "fa-circle";
 
 function sanitizeString(str) {
   str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
   return str.trim();
 }
+
+//insert icon before input
+input.insertAdjacentHTML('beforebegin', addIcon);
 
 let app = {
   todos: [],
@@ -22,37 +32,42 @@ let app = {
     task.done = false;
     if (item) { //makes sure input is not blank
       this.todos.push(task);
+      this.displayList(); // refreshes list
     } else { alert("A todo item can not be blank!") };
-    this.displayList(); // refreshes list
     itemInput.value = ""; // clears out input box
   },
   displayList: function() {
     this.todos.forEach(item => {
+      let returnChecked = () => {
+        if (!item.done) {
+          return unchecked;
+        } else return checked;
+      };
       // for each to do item, display it as a li
-      let text = `<li><i id="checkbox" class="far fa-circle"></i> ${sanitizeString(item.content)}</li>`;
+      let text = `<li><i id="${item.id}" class="checkbox far ${returnChecked()}"></i><span> ${sanitizeString(item.content)}</span></li>`;
       ul.insertAdjacentHTML("beforeend", text)
     });
   },
-  markComplete: function() {
-    
+  clearList: function() {
+    ul.innerHTML = '';
+    this.todos = [];
   }
 }
 
-document.addEventListener('click', function (e) {
-  if (e.target && e.target.id == 'checkbox') {
-    e.target.classList.toggle('fa-check-circle');
-    e.target.classList.toggle('fa-circle');
+document.addEventListener('click', function(event) {
+  const element = event.target;
+  if (element && element.classList.contains("checkbox")) {
+    element.classList.toggle(checked);
+    element.classList.toggle(unchecked);
+    app.todos[element.id].done = false ? false : true;
   }
 });
 
-ul.addEventListener('click',function(event) {
-  console.log(event.target);
+
+clearButton.addEventListener('click', function() {
+  app.clearList();
 });
 
-
-inputButton.addEventListener('click', function () {
-  app.addItem(itemInput.value); //adds whatever is in input box
-});
 
 document.addEventListener("keyup", function (event) {
   if (event.keyCode == 13) {
